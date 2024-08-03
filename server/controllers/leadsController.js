90de3bac546523c606f3de5186a67dacbf4cd230
BewellDental <165155886+Asglobalsofttech23@users.noreply.github.com>
2024-08-03 10:20:49 +0530

    Update leadsController.js

--- server/controllers/leadsController.js
+++ server/controllers/leadsController.js
@@ -441,29 +441,49 @@ router.put('/updateFlwLeadForEmp/:id',(req,res)=>{
     
 // =================================================================================
 
-    router.get('/leadsCountForEmpDashboard/:empId', (req, res) => {
-        const empId = req.params.empId;
-        const currentDate = moment().format("YYYY-MM-DD");
-        const getData = `
-            SELECT 
-                COUNT(CASE WHEN reminder_date = '${currentDate}' THEN 1 END) AS reminder_date_count,
-                COUNT(CASE WHEN DATE(created_at) = '${currentDate}' THEN 1 END) AS created_at_count 
-            FROM 
-                following_leads
-            WHERE 
-                emp_id = '${empId}'`;
-    
-        db.query(getData, (getErr, getRes) => {
-            if (getErr) {
-                res.status(500).json({ message: "Internal server error." });
-            } else if (getRes.length === 0) {
-                res.status(404).json({ message: "Data not found." });
-            } else {
-                res.status(200).json(getRes[0]);
-            }
-        });
+  
+router.get('/leadsCountForEmpDashboard/:empId', (req, res) => {
+    const empId = req.params.empId;
+    const currentDate = moment().format("YYYY-MM-DD");
+    const getData = `
+        SELECT 
+            COUNT(CASE WHEN reminder_date = '${currentDate}' THEN 1 END) AS reminder_date_count,
+            COUNT(CASE WHEN DATE(created_at) = '${currentDate}' THEN 1 END) AS created_at_count 
+        FROM 
+            following_leads
+        WHERE 
+            emp_id = '${empId}'`;
+
+    db.query(getData, (getErr, getRes) => {
+        if (getErr) {
+            res.status(500).json({ message: "Internal server error." });
+        } else if (getRes.length === 0) {
+            res.status(404).json({ message: "Data not found." });
+        } else {
+            res.status(200).json(getRes[0]);
+        }
     });
-    
+});
+
+// Endpoint to fetch detailed lead data for today
+router.get('/todayFollowLeadsDetails/:empId', (req, res) => {
+    const empId = req.params.empId;
+    const currentDate = moment().format("YYYY-MM-DD");
+    const getDetails = `
+        SELECT leads_name, leads_mobile, leads_email, product_name
+        FROM following_leads
+        WHERE emp_id = '${empId}' AND DATE(created_at) = '${currentDate}'`;
+
+    db.query(getDetails, (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error fetching data');
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
 
     router.get('/callNotAttended', (req, res) => {
         console.log("rrrr");
